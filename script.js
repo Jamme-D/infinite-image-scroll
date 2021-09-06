a58b4ce3e7d75fe5fb9bd0670e3c00df744d0969
Dallas Jamme <dallas@MacBook-Air.local>
2021-09-06 12:57:26 -0700

    update

---
+++ script.js
@@ -0,0 +1,82 @@
+const imgContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+const count = 25;
+const apiKey = 'eAZkbkq0wtWlan5NR-MFOViIHnvvhqsLl8g6XtCbdGI';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper function to set attributes on DOM elements and clean up code
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Check to see if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+// Create elements for links & photos, and add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        // Create <img> for photo
+        const img = document.createElement('img');
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        // Event listener to check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
+        // Put <img> inside <a>, then put both inside image-container
+        item.appendChild(img);
+        imgContainer.appendChild(item);
+    });
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+// Check to see if scrolling is reaching the bottom of the page to load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
+
+// On load
+getPhotos();
